10f8ca368a6165e73a16bdeb325e03924f155267
Rigel9802 <luckyrigel9802@gmail.com>
2023-05-17 18:31:34 +0700

    first commit

---
+++ src/test/java/com/railway/belajarrailway/BelajarRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railway.belajarrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
